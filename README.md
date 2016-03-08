131c0481e8eedcf04c96b2dcc0fb0d0d665efbee
Fernando Serena <fernando.serena@centeropenmiddleware.com>
2016-03-08 17:58:05 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+#Agora-Curator
+
+The service responsible for enriching resources that belong to a Linked Data Platform powered by Agora.
+
+Agora-Curator is distributed under the Apache License, version 2.0.
---
+++ agora/curator/daemons/fragment.py
@@ -0,0 +1,214 @@
+"""
+#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#
+  This file is part of the Smart Developer Hub Project:
+    http://www.smartdeveloperhub.org
+
+  Center for Open Middleware
+        http://www.centeropenmiddleware.com/
+#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#
+  Copyright (C) 2015 Center for Open Middleware.
+#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at 
+
+            http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#
+"""
+import logging
+import random
+import time
+import traceback
+from datetime import datetime as dt
+from threading import Thread
+
+from redis.lock import Lock
+
+from agora.stoa.client import get_query_generator
+from agora.stoa.daemons.delivery import build_response
+from agora.stoa.server import app
+from agora.stoa.store import r
+from agora.stoa.store.tables import db
+from agora.stoa.store.triples import cache
+from concurrent.futures.thread import ThreadPoolExecutor
+
+__author__ = 'Fernando Serena'
+
+log = logging.getLogger('agora.curator.daemons.fragment')
+AGORA = app.config['AGORA']
+BROKER = app.config['BROKER']
+ON_DEMAND_TH = float(app.config.get('PARAMS', {}).get('on_demand_threshold', 2.0))
+MIN_SYNC = int(app.config.get('PARAMS', {}).get('min_sync_time', 10))
+MAX_CONCURRENT_FRAGMENTS = int(app.config.get('PARAMS', {}).get('max_concurrent_fragments', 8))
+
+log.info("""Fragment daemon setup:
+                    - On-demand threshold: {}
+                    - Minimum sync time: {}
+                    - Maximum concurrent fragments: {}""".format(ON_DEMAND_TH, MIN_SYNC,
+                                                                 MAX_CONCURRENT_FRAGMENTS))
+
+thp = ThreadPoolExecutor(max_workers=min(8, MAX_CONCURRENT_FRAGMENTS))
+
+log.info('Cleaning fragment locks...')
+fragment_locks = r.keys('*lock*')
+for flk in fragment_locks:
+    r.delete(flk)
+
+log.info('Cleaning fragment pulling flags...')
+fragment_pullings = r.keys('fragments:*:pulling')
+for fpk in fragment_pullings:
+    r.delete(fpk)
+
+
+def fragment_lock(fid):
+    lock_key = 'fragments:{}:lock'.format(fid)
+    return r.lock(lock_key, lock_class=Lock)
+
+
+def __notify_completion(sinks):
+    """
+    Notify the ending of a fragment collection to all related requests
+    :param sinks: Set of dependent sinks
+    :return:
+    """
+
+    for sink in sinks.values():
+        if sink.delivery == 'accepted':
+            sink.delivery = 'ready'
+
+
+def __load_fragment_requests(fid):
+    """
+    Load all requests and their sinks that are related to a given fragment id
+    :param fid: Fragment id
+    :return: A dictionary of sinks of all fragment requests
+    """
+    sinks_ = {}
+    for rid in r.smembers('fragments:{}:requests'.format(fid)):
+        try:
+            sinks_[rid] = build_response(rid).sink
+        except Exception, e:
+            traceback.print_exc()
+            log.warning(e.message)
+            with r.pipeline(transaction=True) as p:
+                p.multi()
+                p.srem('fragments:{}:requests'.format(fid), rid)
+                p.execute()
+    return sinks_
+
+
+def __pull_fragment(fid):
+    tps = r.smembers('fragments:{}:gp'.format(fid))
+    r_sinks = __load_fragment_requests(fid)
+    log.info("""Starting collection of fragment {}:
+                    - GP: {}
+                    - Supporting: ({}) {}""".format(fid, list(tps), len(r_sinks), list(r_sinks.keys())))
+
+    try:
+        prefixes, gen = get_query_generator(*tps,
+                                            broker_host=BROKER['host'],
+                                            agora_host=AGORA['host'],
+                                            broker_port=BROKER['port'],
+                                            agora_port=AGORA['port'], wait=True)
+    except Exception, e:
+        traceback.print_exc()
+        log.error('Scholar is not available')
+        return
+
+    lock = fragment_lock(fid)
+    lock.acquire()
+
+    with r.pipeline(transaction=True) as p:
+        p.multi()
+        p.set('fragments:{}:pulling'.format(fid), True)
+        p.execute()
+
+    db[fid].delete_many({})
+    db.drop_collection(fid)
+
+    lock.release()
+
+    try:
+        for headers, row in gen:
+            try:
+                lock.acquire()
+                db[fid].insert_one(row)
+            except Exception, e:
+                log.warning(e.message)
+                traceback.print_exc()
+            finally:
+                lock.release()
+    except Exception, e:
+        log.error(e.message)
+
+    lock.acquire()
+    try:
+        with r.pipeline(transaction=True) as p:
+            p.multi()
+            sync_key = 'fragments:{}:sync'.format(fid)
+            # Fragment is now synced
+            p.set(sync_key, True)
+            min_durability = int(MIN_SYNC)
+            durability = random.randint(min_durability, min_durability * 2)
+            p.expire(sync_key, durability)
+            log.info('Fragment {} is considered synced for {} s'.format(fid, durability))
+            p.set('fragments:{}:updated'.format(fid), dt.now())
+            p.delete('fragments:{}:pulling'.format(fid))
+            p.execute()
+        if r.scard('fragments:{}:requests'.format(fid)) != len(r_sinks):
+            r_sinks = __load_fragment_requests(fid)
+        __notify_completion(r_sinks)
+    finally:
+        lock.release()
+
+
+def __collect_fragments():
+    registered_fragments = r.scard('fragments')
+    synced_fragments = len(r.keys('fragments:*:sync'))
+    log.info("""Collector daemon started:
+                    - Fragments: {}
+                    - Synced: {}""".format(registered_fragments, synced_fragments))
+
+    futures = {}
+    while True:
+        for fid in filter(
+                lambda x: r.get('fragments:{}:sync'.format(x)) is None and r.get(
+                    'fragments:{}:pulling'.format(x)) is None,
+                r.smembers('fragments')):
+            if fid in futures:
+                if futures[fid].done():
+                    del futures[fid]
+            if fid not in futures:
+                futures[fid] = thp.submit(__pull_fragment, fid)
+        time.sleep(1)
+
+
+def fragment_updated_on(fid):
+    return r.get('fragments:{}:updated'.format(fid))
+
+
+def fragment_on_demand(fid):
+    return r.get('fragments:{}:on_demand'.format(fid))
+
+
+def is_pulling(fid):
+    return r.get('fragments:{}:pulling'.format(fid)) is not None
+
+
+def is_fragment_synced(fid):
+    return fragment_updated_on(fid) is not None
+
+
+def fragment_graph(fid):
+    return cache.get_context('/' + fid)
+
+
+th = Thread(target=__collect_fragments)
+th.daemon = True
+th.start()
